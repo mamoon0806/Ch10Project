8dbbd47b7d9379a90f9e383aba8c9daea17d5702
5921001744 <5921001744@SHS-13-221-025.inst.scpsnet.edu>
2019-09-10 09:15:01 -0400

    .

--- TextAnalyzer/src/edu/seminolestate/textanalyzer/TextAnalyzerApplication.java
+++ TextAnalyzer/src/edu/seminolestate/textanalyzer/TextAnalyzerApplication.java
@@ -1,12 +1,28 @@
 package edu.seminolestate.textanalyzer;
+import java.util.Scanner;
 
 public class TextAnalyzerApplication {
-	public static void main(String[] args) {
+	
+	public static void displayMenu() {
 		System.out.println("1 - Create the text\n"
 				+ "2 - Change the existing text\n"
 				+ "3 - Display statistics for existing text\n"
 				+ "4 - Add new text to existing text\n"
 				+ "5 - Display existing text\n"
 				+ "6 - Exit");
+	}
+	
+	public static void main(String[] args) {
+		displayMenu();
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		
+		if(number == 1) {
+			System.out.println("Enter the new text for the TextAnalyzer object:");
+			String text = sc.next();
+			TextAnalyzer textAnalyzer = new TextAnalyzer(text);
+			displayMenu();
+		}
+		
 	}
 }
